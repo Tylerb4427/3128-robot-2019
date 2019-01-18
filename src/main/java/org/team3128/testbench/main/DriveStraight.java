6eecc8f61b44194c9657ab0d6f96c143fdd2a9c4
Tyler Bovenzi <Tylerb4427@gmail.com>
2019-01-18 15:39:36 -0800

    Created DriveStraight

--- src/main/java/org/team3128/testbench/main/DriveStraight.java
+++ src/main/java/org/team3128/testbench/main/DriveStraight.java
@@ -10,22 +10,6 @@ import org.team3128.common.util.units.Length;
 
 import edu.wpi.first.wpilibj.Joystick;
 
-import org.team3128.common.NarwhalRobot;
-import org.team3128.common.drive.SRXTankDrive;
-import org.team3128.common.hardware.misc.Piston;
-import org.team3128.common.hardware.misc.TwoSpeedGearshift;
-import org.team3128.common.listener.ListenerManager;
-import org.team3128.common.listener.POVValue;
-import org.team3128.common.listener.controllers.ControllerExtreme3D;
-import org.team3128.common.listener.controltypes.Button;
-import org.team3128.common.listener.controltypes.POV;
-import org.team3128.common.narwhaldashboard.NarwhalDashboard;
-import org.team3128.common.util.Constants;
-import org.team3128.common.util.Log;
-import org.team3128.common.util.enums.Direction;
-import org.team3128.common.util.units.Angle;
-import org.team3128.common.util.units.Length;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -102,7 +86,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-public class Antitip extends NarwhalRobot {
+public class DriveStraight extends NarwhalRobot {
     AHRS ahrs;
     public TalonSRX boi1, boi2;
     public ListenerManager listenerLeft, listenerRight;
@@ -173,16 +157,9 @@ public class Antitip extends NarwhalRobot {
     protected void teleopPeriodic() {
     Float ThetaThreshold = (float)10;     
     Float Theta=ahrs.getYaw();
-    //Float Theta=ahrs.getRoll();
-        if(Math.abs(Theta)>ThetaThreshold){
-            boi1.set(ControlMode.PercentOutput,Theta/100);
-            boi2.set(ControlMode.PercentOutput,Theta/100);
-        }
-        else{
-            boi1.set(ControlMode.PercentOutput,0);
-            boi2.set(ControlMode.PercentOutput,0);
-        }
-        Log.debug("Pitch", Float.toString(Theta));
+    boi1.set(ControlMode.PercentOutput,.30+Theta/100);
+    boi2.set(ControlMode.PercentOutput,.30-Theta/100);
+    Log.debug("Pitch", Float.toString(Theta));
     
     }
 
@@ -192,7 +169,7 @@ public class Antitip extends NarwhalRobot {
     }
 
     public static void main(String[] args) {
-        RobotBase.startRobot(Antitip::new);
+        RobotBase.startRobot(DriveStraight::new);
     }
 
 }
